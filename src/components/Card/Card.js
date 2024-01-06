d9d1ec284d2d4a35d235ff2a961b67d0da6baad9
Mohd Azgar Ali <azgar53ali@gmail.com>
2024-01-06 23:39:08 +0530

    updated inline css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import styles from './Card.module.css';
 
 const Card=(props)=>{
     return(
-        <div className={styles.cardDiv}>
+        <div className={styles.cardDiv} style={{flexDirection:'column'}}>
             <img src={props.image} alt={props.imgName}></img>
             <p>{props.city}</p>
         </div>
